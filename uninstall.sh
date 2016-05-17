f05176c806785e10dcad74e1f38049ae85a5f42b
Chris Green <cmgreen210@gmail.com>
2016-05-16 17:02:46 -0700

    Add torch ipynb

--- uninstall.sh
+++ uninstall.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 if [ -d "torch" ] ; then
-    read -p "Are you sure you want to uninstall Torch?" yn
-    case $yn in
-        [Yy]* ) rm -rf "torch"; break;;
-        * ) echo "Uninstall aborted!";;
-    esac
+    read -p "Are you sure you want to uninstall Torch?" -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        rm -rf "torch"
+    else
+        echo "Uninstall aborted!"
+    fi
 else
     echo "Torch not found!"
 fi
