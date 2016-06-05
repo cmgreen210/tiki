3b559215d4a828780dd2b56ff94d2bb0ba14c005
Ubuntu <ubuntu@ip-172-31-37-120.ec2.internal>
2016-06-05 16:51:46 +0000

    Update download and gitignore

--- face_ae/download.sh
+++ face_ae/download.sh
@@ -6,3 +6,6 @@ fi
 
 mkdir data
 (cd data && wget http://tamaraberg.com/faceDataset/faceData.tar.gz && tar -xf faceData.tar.gz && rm faceData.tar.gz)
+
+luarocks install unsup
+luarocks install matio
